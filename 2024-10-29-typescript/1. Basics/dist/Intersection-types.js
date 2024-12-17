4574b17ad11f38d7912c28f98e663ad2bbb6d828
Eutodot <ado.domantas@gmail.com>
2024-12-17 18:11:40 +0200

    generic types, html, other

--- 2024-10-29-typescript/1. Basics/dist/Intersection-types.js
+++ 2024-10-29-typescript/1. Basics/dist/Intersection-types.js
@@ -21,3 +21,46 @@ const add3 = (a, b) => {
     }
     return a + b;
 };
+const printInfo = (employee) => {
+    console.log(employee.name);
+    if ('startDate' in employee) {
+        console.log(employee.startDate);
+    }
+    if ('privileges' in employee) {
+        console.log(employee.privileges);
+    }
+};
+printInfo(employee1);
+const moveAnimal = (animal) => {
+    switch (animal.type) {
+        case 'bird':
+            console.log(animal.flyingSpeed);
+            break;
+        case 'horse':
+            console.log(animal.runningSpeed);
+            break;
+        default:
+    }
+};
+moveAnimal({ type: "bird", flyingSpeed: 12 });
+const userInput = document.getElementById('userInput');
+if (userInput) {
+    userInput.value = "John";
+}
+function add4(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+const answer = add4('text1', 'text 2');
+const answer2 = add4(2, "2");
+const names = [];
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('promise is done');
+    }, 2000);
+});
+promise.then(data => {
+    console.log(data.split(" "));
+});
--- 2024-10-29-typescript/1. Basics/src/Intersection-types.ts
+++ 2024-10-29-typescript/1. Basics/src/Intersection-types.ts
@@ -78,6 +78,110 @@ printInfo(employee1)
 
 
 
+/////////////////// Discriminated union type
+
+
+type Bird = {
+    flyingSpeed: number
+    type: "bird"
+}
+type Horse = {
+    runningSpeed: number
+    type: "horse"
+}
+type Animal = Bird | Horse
+
+const moveAnimal = (animal: Animal) => {
+    // console.log(animal.runningSpeed)     // Neleidzia
+
+    // if ('runningSpeed' in animal){
+    //     console.log(animal.runningSpeed)
+    // }
+
+    switch(animal.type){
+        case 'bird':
+            console.log(animal.flyingSpeed)
+            break
+        case 'horse':
+            console.log(animal.runningSpeed)
+            break
+        default:
+    }
+}
+
+moveAnimal({type: "bird", flyingSpeed: 12})
+
+
+////////////////////// HTML
+
+// const paragraph = document.querySelector('p')!
+
+
+// const paragraph = document.getElementById('paragraph')
+
+
+// const userInput = <HTMLInputElement>document.getElementById('userInput')!    // 1 budas
+
+// const userInput = document.getElementById('userInput')! as HTMLInputElement     // 2 budas
+
+
+
+const userInput = document.getElementById('userInput')    // 3 budas
+
+if (userInput){
+    (userInput as HTMLInputElement).value = "John"
+}
+
+
+
+function add4(a: number, b: number): number;
+function add4(a: string, b: string): string;
+function add4(a: string, b: number): string;
+function add4(a: number, b: string): string;
+function add4(a: Combinable2, b: Combinable2){
+    if (typeof a === 'string' || typeof b === 'string'){
+        return a.toString() + b.toString()
+    }
+
+    return a + b
+}
+
+const answer = add4('text1', 'text 2')
+// answer.split('r')
+
+const answer2 = add4(2, "2")
+// answer2.split('r')
+
+
+
+//////////////////////////////////////// Generic type
+
+
+// const names = ['John', 'Steve']
+// const names = []
+// const names: Array<string> = []
+// const names: Array<number> = []
+// const names: Array<any> = []
+// const names: Array<string | number> = []
+
+
+const names: string[] = []
+// const names: number[] = []
+// const names: any[] = []
+// const names: string[] | number[] = []
+
+// names[0].split('s')
+
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('promise is done')
+    }, 2000)
+})
+
+promise.then(data => {
+    console.log(data.split(" "))
+})
 
 
 
@@ -90,5 +194,14 @@ printInfo(employee1)
 
 
 
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////  GRYZTI PRIE INDEX PROPERTIES
 
 
